7e931e8110cc4d8ca63660303c9e0b2d9c05fe8e
Luis <Luistejeda0011@gmail.com>
2022-05-15 23:27:36 -0300

    NOT NULL added in user model

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,21 +1,18 @@
-import { Column, DataType, Model, NotEmpty, Table } from "sequelize-typescript"
+import { Column, DataType, Model, NotEmpty, Table, AllowNull } from "sequelize-typescript"
 
 export enum role {
     Admin = 'admin',
     Poster = 'poster',
     Visitor = 'visitor'
 }
-
 export interface IUser {
-    id?: number | null
+    User_ID?: number | null
     first_name: string
     last_name: string
     role: role,
     email: string
     password: string
-
 }
-
 @Table(
     {
         tableName: 'user',
@@ -30,33 +27,38 @@ export default class User extends Model implements IUser {
         primaryKey: true,
         autoIncrement: true
     })
-    id: number | null | undefined
+    User_ID: number | null | undefined
 
     @NotEmpty
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
     first_name!: string
 
     @NotEmpty
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
     last_name!: string
 
     @NotEmpty
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
     role!: role
 
     @NotEmpty
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
     email!: string
 
     @NotEmpty
+    @AllowNull(false)
     @Column({
         type: DataType.STRING(100)
     })
