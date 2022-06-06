857034907c50bf96b10cba9b8d7b91fc958ae14c
Luis <Luistejeda0011@gmail.com>
2022-06-06 17:17:28 -0300

    Config to Heroku

--- build/config/authConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secret: process.env.AUTH_SECRET,
-    expires: process.env.AUTH_EXPIRES || '1d',
-    rounds: process.env.AUTH_ROUNDS || 8
-};
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import app from './libs/middlewares';
-import db from './models/db.model'
+import db from "./models/db.model";
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`App is running at http://localhost:${PORT} in development mode
-Press CTRL-C to stop`)
-
-    db.sequelize.authenticate().then(async () => {
-        console.log("database connected")
-        try {
-            await db.sequelize.sync({ force: false })
-        } catch (error: any) {
-            console.log(error.message)
-        }
-
-    }).catch((e: any) => {
-        console.log(e.message)
+  console.log(`App is running at http://localhost:${PORT} in development mode
+Press CTRL-C to stop`);
+  db.sequelize
+    .authenticate()
+    .then(async () => {
+      console.log("database connected");
+      try {
+        await db.sequelize.sync({ force: false });
+      } catch (error: any) {
+        console.log(error.message);
+      }
     })
-})
+    .catch((e: any) => {
+      console.log(e.message);
+    });
+});
